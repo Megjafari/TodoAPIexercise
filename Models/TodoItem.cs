7f37aa33403ba7d09d7e9fd2fd8a7c5069fa8ec3
Meghdad Jafari <megberserker@gmail.com>
2026-01-29 19:21:46 +0100

    Move backend files to /backend and frontend files to /frontend

--- Models/TodoItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TodoAPIexercise.Models
-{
-    public class TodoItem
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public bool IsCompleted { get; set; }
-    }
-}
